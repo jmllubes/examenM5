fed4243fa5eb85069c57c357efa00700d407f728
Toni López Llaó <tonilopez450@gmail.com>
2021-12-17 12:40:30 +0100

    Update primer.java

--- src/basics/primer.java
+++ src/basics/primer.java
@@ -1,10 +1,11 @@
+// Lopez
 package basics;
 
 public class primer {
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println("hola món");
+	System.out.println("hola mÃ³n");
 
 
 }
