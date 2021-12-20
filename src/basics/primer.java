7d56331ee8d83d8ce2fdb624dd98284299808a80
SantiagoOB <santiagospina1608@gmail.com>
2021-12-20 10:09:46 +0100

    Update

--- src/basics/primer.java
+++ src/basics/primer.java
@@ -6,7 +6,7 @@ public class primer {
 	//Santiago Ospina
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println("hola món");
+	System.out.println("Santiago Ospina");
 
 
 }
